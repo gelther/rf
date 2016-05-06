26e53daa5f245d2a519b7d6f225c48720b928606
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-06 15:45:02 +0200

    Code consistency changes for includes/receiptful-helper-functions.php

--- includes/receiptful-helper-functions.php
+++ includes/receiptful-helper-functions.php
@@ -9,11 +9,10 @@ if ( ! function_exists( 'wc_get_coupon_by_code' ) ) {
 	 *
 	 * Get the coupon ID by the coupon code.
 	 *
-	 * @param	string		$coupon_code	Code that is used as coupon code.
-	 * @return	int|bool					WP_Post ID if coupon is found, otherwise False.
+	 * @param  string   $coupon_code Code that is used as coupon code.
+	 * @return int|bool              WP_Post ID if coupon is found, otherwise False.
 	 */
 	function wc_get_coupon_by_code( $coupon_code ) {
-
 		global $wpdb;
 
 		$coupon_id = $wpdb->get_var( $wpdb->prepare( apply_filters( 'woocommerce_coupon_code_query', "
@@ -31,7 +30,6 @@ if ( ! function_exists( 'wc_get_coupon_by_code' ) ) {
 		}
 
 	}
-
 }
 
 
@@ -42,30 +40,28 @@ if ( ! function_exists( 'wc_get_random_products' ) ) {
 	 *
 	 * Get random WC product IDs.
 	 *
-	 * @param	int		$limit	Number of products to return
-	 * @return	array			List of random product IDs.
+	 * @param  int   $limit Number of products to return
+	 * @return array        List of random product IDs.
 	 */
 	function wc_get_random_products( $limit = 2 ) {
-
 		$product_args = array(
-			'fields'			=> 'ids',
-			'post_type'			=> 'product',
-			'post_status'		=> 'publish',
-			'posts_per_page'	=> $limit,
-			'orderby'			=> 'rand',
-			'meta_query'		=> array(
+			'fields'         => 'ids',
+			'post_type'      => 'product',
+			'post_status'    => 'publish',
+			'posts_per_page' => $limit,
+			'orderby'        => 'rand',
+			'meta_query'     => array(
 				array(
-					'meta_key'	=> '_thumbnail_id',
-					'compare'	=> 'EXISTS',
+					'meta_key' => '_thumbnail_id',
+					'compare'  => 'EXISTS',
 				),
 			)
 		);
-		$products = get_posts( $product_args );
+		$products     = get_posts( $product_args );
 
 		return $products;
 
 	}
-
 }
 
 
@@ -86,10 +82,8 @@ if ( ! function_exists( 'wc_get_product' ) ) {
 	 * @codeCoverageIgnore
 	 */
 	function wc_get_product( $product_id ) {
-
 		return get_product( $product_id );
 	}
-
 }
 
 
@@ -101,7 +95,6 @@ if ( ! function_exists( 'wc_get_order' ) ) {
 	 * @codeCoverageIgnore
 	 */
 	function wc_get_order( $order ) {
-
 		$order_id = 0;
 
 		if ( $order instanceof WP_Post ) {
@@ -114,7 +107,6 @@ if ( ! function_exists( 'wc_get_order' ) ) {
 
 		return new WC_Order( $order_id );
 	}
-
 }
 
 
@@ -131,7 +123,6 @@ if ( ! function_exists( 'wc_tax_enabled' ) ) {
 	function wc_tax_enabled() {
 		return get_option( 'woocommerce_calc_taxes' ) === 'yes';
 	}
-
 }
 
 
@@ -143,27 +134,26 @@ if ( ! function_exists( 'wc_tax_enabled' ) ) {
  * @since 1.2.2
  */
 function receiptful_clear_unused_coupons() {
-
 	$expired_coupons = new WP_Query( array(
-		'post_type' => 'shop_coupon',
-		'fields' => 'ids',
+		'post_type'      => 'shop_coupon',
+		'fields'         => 'ids',
 		'posts_per_page' => 1000,
-		'meta_query' => array(
+		'meta_query'     => array(
 			array(
-				'key' => 'receiptful_coupon',
+				'key'     => 'receiptful_coupon',
 				'compare' => '=',
-				'value' => 'yes',
+				'value'   => 'yes',
 			),
 			array(
-				'key' => 'expiry_date',
+				'key'     => 'expiry_date',
 				'compare' => '<',
-				'type' => 'DATE',
-				'value' => date_i18n( 'Y-m-d', strtotime( '-7 days' ) ),
+				'type'    => 'DATE',
+				'value'   => date_i18n( 'Y-m-d', strtotime( '-7 days' ) ),
 			),
 			array(
-				'key' => 'usage_count',
+				'key'     => 'usage_count',
 				'compare' => '=',
-				'value' => '0',
+				'value'   => '0',
 			),
 		),
 	) );
@@ -172,9 +162,7 @@ function receiptful_clear_unused_coupons() {
 	foreach ( $expired_coupons->get_posts() as $post_id ) {
 		wp_trash_post( $post_id );
 	}
-
 }
-
 
 /**
  * Add Receiptful version endpoint.
@@ -185,12 +173,11 @@ function receiptful_clear_unused_coupons() {
  * @since 1.2.5
  */
 function receiptful_add_active_endpoint() {
-
 	if ( isset( $_GET['receiptful_version'] ) ) {
 		wp_send_json( Receiptful()->version );
 	}
-
 }
+
 if ( isset( $_GET['receiptful_version'] ) ) {
 	add_action( 'init', 'receiptful_add_active_endpoint' );
 }
