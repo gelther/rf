85ff757c0e2473c797f9e35f2f74d157ee2e7a83
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-06 15:47:38 +0200

    Code consistency changes for includes/class-receiptful-recommendations.php

--- includes/class-receiptful-recommendations.php
+++ includes/class-receiptful-recommendations.php
@@ -26,29 +26,28 @@ class Receiptful_Recommendations {
 
 	}
 
-
 	/**
 	 * Get recommendations.
 	 *
-	 * Get the recommendations HTML.
+	 *  Get the recommendations HTML.
 	 *
 	 * @since 1.1.6
 	 *
-	 * @param	array	$args	List of arguments for the recommendations.
-	 * @return	string			HTML code with the appropriate attributes.
+	 * @param  array  $args List of arguments for the recommendations.
+	 * @return string       HTML code with the appropriate attributes.
 	 */
 	public function get_recommendations( $args = array() ) {
 
 		$args = wp_parse_args( $args, array(
-			'name'		  			=> null,
-			'show_header' 			=> null,
-			'header_text' 			=> null,
-			'header_type' 			=> null,
-			'show_title'  			=> null,
-			'show_price'  			=> null,
-			'price_format'			=> null,
-			'number_of_products'	=> null,
-			'styles'				=> null,
+			'name'               => null,
+			'show_header'        => null,
+			'header_text'        => null,
+			'header_type'        => null,
+			'show_title'         => null,
+			'show_price'         => null,
+			'price_format'       => null,
+			'number_of_products' => null,
+			'styles'             => null,
 		) );
 
 		if ( 'false' == $args['show_title'] || '0' == $args['show_title'] ) :
@@ -76,15 +75,14 @@ class Receiptful_Recommendations {
 
 	}
 
-
 	/**
 	 * Display recommendations.
 	 *
-	 * Display the recommendations HTML.
+	 *  Display the recommendations HTML.
 	 *
 	 * @since 1.1.6
 	 *
-	 * @param	array	$args	Recommendation arguments.
+	 * @param array $args Recommendation arguments.
 	 */
 	public function display_recommendations( $args = array() ) {
 
@@ -100,26 +98,25 @@ class Receiptful_Recommendations {
 	 *
 	 * @since 1.1.6
 	 *
-	 * @param	array	$args	Recommendation arguments.
-	 * @return	string			HTML for displaying recommendations.
+	 * @param  array  $args Recommendation arguments.
+	 * @return string       HTML for displaying recommendations.
 	 */
 	public function recommendation_shortcode( $atts = array() ) {
 
 		$args = shortcode_atts( array(
-			'name'		  			=> null,
-			'show_header' 			=> null,
-			'header_text' 			=> null,
-			'header_type' 			=> null,
-			'show_title'  			=> null,
-			'show_price'  			=> null,
-			'price_format'			=> null,
-			'number_of_products'	=> null,
-			'styles'				=> null,
+			'name'               => null,
+			'show_header'        => null,
+			'header_text'        => null,
+			'header_type'        => null,
+			'show_title'         => null,
+			'show_price'         => null,
+			'price_format'       => null,
+			'number_of_products' => null,
+			'styles'             => null,
 		), $atts );
 
 		return $this->get_recommendations( $args );
 
 	}
-
 
 }
