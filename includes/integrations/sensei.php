386d0d897b51f5e5ae9273803e2659f7fbd5b174
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-06 15:44:55 +0200

    Code consistency changes for includes/integrations/sensei.php

--- includes/integrations/sensei.php
+++ includes/integrations/sensei.php
@@ -18,7 +18,6 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  * @return bool True when WooThemes Sensei is active, false otherwise.
  */
 function receiptful_is_sensei_active() {
-
 	$active = false;
 	if ( in_array( 'woothemes-sensei/woothemes-sensei.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 		$active = true;
@@ -27,7 +26,6 @@ function receiptful_is_sensei_active() {
 	}
 
 	return $active;
-
 }
 
 /**
@@ -35,14 +33,13 @@ function receiptful_is_sensei_active() {
  *
  * Add Sensei course links to the receipt when the Sensei plugin is used.
  *
- * @param $urls
- * @param $item
- * @param $order_id
+ * @param        $urls
+ * @param        $item
+ * @param        $order_id
  * @return array
  */
 function receiptful_sensei_add_course_links( $urls, $item, $order_id ) {
-
-	$order = wc_get_order( $order_id );
+	$order       = wc_get_order( $order_id );
 	$order_items = $order->get_items();
 
 	// exit early if not wc-completed or wc-processing
@@ -55,43 +52,43 @@ function receiptful_sensei_add_course_links( $urls, $item, $order_id ) {
 	}
 
 	$product_id = ! empty( $item['variation_id'] ) ? $item['variation_id'] : $item['product_id'];
-	$user_id = get_post_meta( $order->id, '_customer_user', true );
+	$user_id    = get_post_meta( $order->id, '_customer_user', true );
 
 	if ( ! $user_id ) {
 		return $urls;
 	}
 
 	// Get all courses for product
-	$args = array(
+	$args    = array(
 		'posts_per_page' => -1,
-		'post_type' => 'course',
-		'meta_query' => array(
+		'post_type'      => 'course',
+		'meta_query'     => array(
 			array(
-				'key' => '_course_woocommerce_product',
+				'key'   => '_course_woocommerce_product',
 				'value' => $product_id
 			)
 		),
-		'orderby' => 'menu_order date',
-		'order' => 'ASC',
+		'orderby'        => 'menu_order date',
+		'order'          => 'ASC',
 	);
 	$courses = get_posts( $args );
 
-	if ( empty( $courses) ) {
+	if ( empty( $courses ) ) {
 		return $urls;
 	}
 
-	foreach( $courses as $course ) {
+	foreach ( $courses as $course ) {
 
-		$title = $course->post_title;
+		$title  = $course->post_title;
 		$urls[] = array(
-			'key' => sprintf( __( 'View course: %1$s', 'receiptful-for-woocommerce' ), $title ),
+			'key'   => sprintf( __( 'View course: %1$s', 'receiptful-for-woocommerce' ), $title ),
 			'value' => esc_url( get_permalink( $course->ID ) ),
 		);
 	}
 
 	return $urls;
-
 }
+
 if ( receiptful_is_sensei_active() ) {
 	add_filter( 'receiptful_get_download_urls', 'receiptful_sensei_add_course_links', 10, 3 );
 }
